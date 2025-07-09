2f08201bdeca592baf9e48b8089754d893ca86fe
Yanisa0000 <Yanisab2909@gmail.com>
2025-07-09 09:48:10 +0700

    Conditional-Python
    เรื่องนี้ทำให้เราได้เข้าใจการใช้ If-else มากขึ้นแล้วพร้องเอาไปต่อยอดต่อไป

---
+++ Conditional-Python/02-even_odd.py
@@ -0,0 +1,6 @@
+score = int(input())
+
+if score %2 == 0:
+    print("Pass")
+else:
+    print("Fail")
---
+++ Conditional-Python/04- Grading.py
@@ -0,0 +1,27 @@
+Frist = int(input("คะแนนเก็บ: "))
+Mit = int(input("คะแนนเก็บ: "))
+Final = int(input("คะแนนเก็บ: "))
+
+score = Frist + Mit + Final
+
+if (0 <= Frist <= 30) and (0 <= Mit <= 30) and (0 <= Final <= 40):
+    if score >= 80:
+     print("A")
+    elif score >= 75:
+     print("B+")
+    elif score >= 70:
+     print("B")
+    elif score >= 65:
+     print("C+")
+    elif score >= 60:
+     print("C")
+    elif score >= 55:
+     print("D+")
+    elif score >= 50:
+     print("D") 
+    elif score >= 0:
+     print("F") 
+    else:
+     print("Fail")
+else :
+    print("input again")
